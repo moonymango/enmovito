6c1114818a7ffd1781332f76aacb877eecc297b1
moonymango <moonymango@gmail.com>
2024-10-04 18:40:59 +0200

    .

--- test.py
+++ test.py
@@ -1,36 +1,29 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
- 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.setGeometry(400, 400, 500, 300)
-        self.setWindowTitle("CodersLegacy")
-        self.initUI()
- 
-    def initUI(self):
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText('hello window')
- 
-        self.btn = QtWidgets.QPushButton(self)
-        self.btn.setText('hello button')
-        self.btn.move(100, 100)
-        self.btn.clicked.connect(self.onClick)
- 
-    def onClick(self):
-        self.label.setText('hello button clicked')
-        self.update()
- 
-    def update(self):
-        self.label.adjustSize()
+from window import Ui_MainWindow
 
 
+class App(Ui_MainWindow):
+    def __init__(self, win):
+        super().__init__()
+        self.setupUi(win)
+        #self.pushButton.clicked.connect(self.on_click)
+
+    def on_click(self):
+        print('PyQt5 button click')
     
 
 def window():
     app = QApplication(sys.argv)
-    win = MyWindow()
+
+    #with open('SyNet.qss', 'r') as f:
+    #    app.setStyleSheet(f.read())
+
+    win = QMainWindow()
+    
+    myapp = App(win)
+
     win.show()
     sys.exit(app.exec_())
      
