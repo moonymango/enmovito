e491c3e11dc9698d2f285b79af7954f04ca502cb
moonymango <moonymango@gmail.com>
2024-10-12 09:05:50 +0200

    .

--- src/test.py
+++ src/test.py
@@ -2,7 +2,8 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 from window import Ui_MainWindow
-
+import pyqtgraph as pg
+import numpy as np
 
 class App(Ui_MainWindow):
     def __init__(self, win):
@@ -10,8 +11,25 @@ class App(Ui_MainWindow):
         self.setupUi(win)
         self.pushButton.clicked.connect(self.on_click)
 
+        # Create a plot widget
+        self.plot_widget = pg.PlotWidget()
+        
+        # Add the plot widget to the central widget layout
+        self.centralwidget.layout().addWidget(self.plot_widget)
+        
+        # Plot some data
+        self.plot_data()
+
     def on_click(self):
         print('PyQt5 button click')
+
+    def plot_data(self):
+        # Generate some data
+        x = np.linspace(0, 10, 100)
+        y = np.sin(x)
+        
+        # Plot the data
+        self.plot_widget.plot(x, y)
 
 
 def window():
